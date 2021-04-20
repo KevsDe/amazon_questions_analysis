d715cada939c2818380dd466dd6f69cb7089f8ac
kevsde <kevin.careaga.bear@gmail.com>
2021-04-20 23:09:56 +0200

    web scraping working

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from services.questions_scraper import AmzQuestionsScraper
+from time import sleep
+asins = ['B07NQKTVH5', 'B08DG68P6B']
+questions_list = []
 
-questions_1 = AmzQuestionsScraper('B07NQKTVH5')
-questions_1.get_questions()
-print(questions_1.questions_list)
+for asin in asins:
+    questions_1 = AmzQuestionsScraper(asin)
+    for x in range(1, questions_1.total_pages + 1):
+        sleep(5)
+        for y in questions_1.get_questions(x):
+            questions_list.append(y.text)
+    questions_1.quit_driver()
+
+print(questions_list)
--- services/questions_scraper.py
+++ services/questions_scraper.py
@@ -1,28 +1,31 @@
 from selenium import webdriver
 import re
 from math import ceil
-from time import sleep
-import pandas as pd
+import itertools
+import csv
 
 chrome_driver = '/home/kevin/development/chromedriver'
 
 
 class AmzQuestionsScraper:
     def __init__(self, asin: str):
+        self.asin = asin
         self.driver = webdriver.Chrome(chrome_driver)
         self.driver.get(f'https://www.amazon.co.uk/ask/questions/asin/{asin}/')
         self.cookies = self.driver.find_element_by_css_selector('#sp-cc-accept')
         self.cookies.click()
-        self.questions_list = []
+        self.total_pages = self.get_total_pages()
 
-    def get_questions(self):
+    def get_total_pages(self):
         number = self.driver.find_element_by_xpath('//*[@id="a-page"]/div[1]/div[5]/table/tbody/tr/td[1]/div/span').text
         total_questions = int(re.findall(r' \d* ', number)[0].strip())
         total_pages = ceil(total_questions / 10)
-        for x in range(total_pages):
-            sleep(2)
-            questions = self.driver.find_elements_by_css_selector('.a-declarative')
-            self.questions_list.append(questions)
-            next_page = self.driver.find_element_by_css_selector('.a-last')
-            next_page.click()
+        return total_pages
 
+    def get_questions(self, idx):
+        self.driver.implicitly_wait(5)
+        self.driver.get(f'https://www.amazon.co.uk/ask/questions/asin/{self.asin}/{idx}/')
+        return self.driver.find_elements_by_css_selector('.a-declarative')
+
+    def quit_driver(self):
+        self.driver.quit()
